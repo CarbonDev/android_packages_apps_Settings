0fbb874368c5461a10e228b768415339cdd3d3d3
slickrick <rsalinas1@mail.usf.edu>
2013-02-10 23:07:57 -0500

    Update Pie Settings again

--- src/com/android/settings/paranoid/Pie.java
+++ src/com/android/settings/paranoid/Pie.java
@@ -34,6 +34,7 @@ import android.provider.Settings.SettingNotFoundException;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.Utils;
+import com.android.settings.util.Helpers;
 import net.margaritov.preference.colorpicker.ColorPickerPreference;
 
 public class Pie extends SettingsPreferenceFragment
