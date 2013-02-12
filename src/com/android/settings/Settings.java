2ca6c9465702c5326e9f0a932af1a2efaf57dcde
Tylar Overturf <toverturf@gmail.com>
2013-02-12 15:09:23 -0700

    Add back rambar.

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -852,4 +852,5 @@ public class Settings extends PreferenceActivity
     public static class AndroidBeamSettingsActivity extends Settings { /* empty */ }
     public static class WifiDisplaySettingsActivity extends Settings { /* empty */ }
     public static class DreamSettingsActivity extends Settings { /* empty */ }
+    public static class ASSRamBarActivity extends Settings { /* empty */ }
 }
