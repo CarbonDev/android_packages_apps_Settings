37ffd4dc553c92d35dc66b86d0ebafefba204536
Dirk Rettschlag <dirk.rettschlag@gmail.com>
2013-03-04 20:59:12 +0100

    Add check for sw600DP screens
    This is used to show options based on screen size.
    
    Change-Id: If99180517cb9e4a4729af11f57b7d4049c3cb10f
    Signed-off-by: Dirk Rettschlag <dirk.rettschlag@gmail.com>
    
    Conflicts:
    	src/com/android/settings/SettingsPreferenceFragment.java
    	src/com/aokp/romcontrol/fragments/Lockscreens.java

--- src/com/android/settings/SettingsPreferenceFragment.java
+++ src/com/android/settings/SettingsPreferenceFragment.java
@@ -31,6 +31,7 @@ import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
 import android.provider.Settings;
 import android.text.TextUtils;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -88,6 +89,13 @@ public class SettingsPreferenceFragment extends PreferenceFragment implements Di
     public static boolean isTablet(Context context) {
         return Settings.System.getInt(context.getContentResolver(),
                 Settings.System.CURRENT_UI_MODE,0) == 1;
+    }
+
+    public static boolean isSW600DPScreen(Context context) {
+        DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
+        int widthPixels = displayMetrics.widthPixels;
+        float density = displayMetrics.density;
+        return ((widthPixels / density) >= 600);
     }
 
     public void setTitle(int resId) {
