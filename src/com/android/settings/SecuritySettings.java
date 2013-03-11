f4107ebf26c42df9eb73d564b3af9da74c867940
fuzz <fuzzzzzzzzzzzzzzzzzz@gmail.com>
2013-03-11 12:45:02 +0900

    LockscreenPattern: Fix toggle dots/error pattern visibility
    This fix the issues dot/error pattern state doesn't save.
    also cleanup redundant code.
    
    Change-Id: Iebaac9679ad9c621d62effd700689485db7c252a

--- src/com/android/settings/SecuritySettings.java
+++ src/com/android/settings/SecuritySettings.java
@@ -261,15 +261,6 @@ public class SecuritySettings extends SettingsPreferenceFragment
         }
 
         if (isCmSecurity) {
-            // visible pattern
-            mVisiblePattern = (CheckBoxPreference) root.findPreference(KEY_VISIBLE_PATTERN);
-
-            // visible error pattern
-            mVisibleErrorPattern = (CheckBoxPreference) root.findPreference(KEY_VISIBLE_ERROR_PATTERN);
-
-            // visible dots
-            mVisibleDots = (CheckBoxPreference) root.findPreference(KEY_VISIBLE_DOTS);
-
             // lock instantly on power key press
             mPowerButtonInstantlyLocks = (CheckBoxPreference) root.findPreference(
                     KEY_POWER_INSTANTLY_LOCKS);
@@ -339,6 +330,12 @@ public class SecuritySettings extends SettingsPreferenceFragment
 
         // visible pattern
         mVisiblePattern = (CheckBoxPreference) root.findPreference(KEY_VISIBLE_PATTERN);
+
+        // visible error pattern
+        mVisibleErrorPattern = (CheckBoxPreference) root.findPreference(KEY_VISIBLE_ERROR_PATTERN);
+
+        // visible dots
+        mVisibleDots = (CheckBoxPreference) root.findPreference(KEY_VISIBLE_DOTS);
 
         // lock instantly on power key press
         mPowerButtonInstantlyLocks = (CheckBoxPreference) root.findPreference(
