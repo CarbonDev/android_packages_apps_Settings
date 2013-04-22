048f079fa3681a7c07b9a77858cd4f3b9fe84119
Stevespear426 <stephen.k.spear@gmail.com>
2013-04-22 16:51:50 +0200

    RC: NavBar fix some selection being off and refresh buttons
    Change-Id: Ibfdc3c1f36f2f1ab59b154524ed477b1b6f4830a

--- src/com/android/settings/carbon/Navbar.java
+++ src/com/android/settings/carbon/Navbar.java
@@ -366,6 +366,7 @@ public class Navbar extends SettingsPreferenceFragment implements
                         Settings.System.NAVIGATION_CUSTOM_APP_ICONS[1], "");
                 Settings.System.putString(mContentRes,
                         Settings.System.NAVIGATION_CUSTOM_APP_ICONS[2], "");
+                loadButtons();
                 refreshSettings();
                 return true;
             default:
@@ -805,9 +806,9 @@ public class Navbar extends SettingsPreferenceFragment implements
                 mPicker.pickShortcut();
         } else { // This should be any other defined action.
             if (button.getPickLongPress()) {
-                button.setLongPress(AwesomeConstants.AwesomeActions()[command]);
+                button.setLongPress(mActionCodes[command]);
             } else {
-                button.setClickAction(AwesomeConstants.AwesomeActions()[command]);
+                button.setClickAction(mActionCodes[command]);
             }
         }
         refreshButtons();
