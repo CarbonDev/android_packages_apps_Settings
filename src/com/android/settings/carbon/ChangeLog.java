b5f9c6940fb57a873383ab94eab435495ad854d1
helicopter88 <johnlemon.lol@gmail.com>
2013-06-22 14:19:54 +0100

    ChangeLog: Launch my app instead of a TextView Signed-off-by: helicopter88 <johnlemon.lol@gmail.com>
    Change-Id: Ie65a58d5dbb724565e0cd25118868c7cae6e3516

--- src/com/android/settings/carbon/ChangeLog.java
+++ src/com/android/settings/carbon/ChangeLog.java
@@ -30,50 +30,20 @@ import android.view.View;
 import android.widget.ScrollView;
 import android.widget.TextView;
 import android.view.ViewGroup;
-
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import android.content.Intent;
+import android.content.ComponentName;
+import android.content.Context;
 
 import com.android.internal.app.AlertActivity;
 import com.android.internal.app.AlertController;
 
 public class ChangeLog extends Fragment {
 
-    private static final String CHANGELOG_PATH = "/system/etc/changelog.txt";
-
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-        InputStreamReader inputReader = null;
-        StringBuilder data = null;
-        try {
-            data = new StringBuilder(2048);
-            char tmp[] = new char[2048];
-            int numRead;
-            inputReader = new FileReader(CHANGELOG_PATH);
-            while ((numRead = inputReader.read(tmp)) >= 0) {
-                data.append(tmp, 0, numRead);
-            }
-        } catch (IOException e) {
-            data.append(this.getString(R.string.changelog_error));
-        } finally {
-            try {
-                if (inputReader != null) {
-                    inputReader.close();
-                }
-            } catch (IOException e) {
-            }
-        }
-
-        TextView textView = new TextView(getActivity());
-        textView.setText(data.toString());
-
-        final ScrollView scrollView = new ScrollView(getActivity());
-        scrollView.addView(textView);
-
-        return scrollView;
-    }
+    public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    Intent intent = new Intent(Intent.ACTION_MAIN);
+	    intent.setComponent(new ComponentName("com.helicopter88.changelog","com.helicopter88.changelog.MainActivity"));
+	    startActivity(intent);
+	}
 }
