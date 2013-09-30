1d94b9b9e4bcabeab60f7b25c73978ecbddb08a8
Jorge Ruesga <jorge@ruesga.com>
2013-10-01 01:06:14 +0200

    Performance profiles
    This changes adds support for custom performance profiles through overlays. Supported
    devices should define config_perf_profile_prop with the proper system property to be set
    and fill an init.rc file with the appropiate handlers for every defined performance profile.
    
    Patchset 2: Rebased
                New simplified strings
    Patchset 3: Fix build
    Patchset 4: Rebased
    
    Change-Id: I5a6612db94da3c09d8ec2e8920bf9516f62d0024
    Signed-off-by: Jorge Ruesga <jorge@ruesga.com>

--- src/com/android/settings/carbon/PerformanceSettings.java
+++ src/com/android/settings/carbon/PerformanceSettings.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2012 The CyanogenMod Project
+ * Copyright (C) 2013 The CyanogenMod Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,11 +18,13 @@ package com.android.settings.carbon;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.os.SystemProperties;
 import android.preference.CheckBoxPreference;
 import android.preference.Preference;
 import android.preference.PreferenceScreen;
+import android.text.TextUtils;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
@@ -33,10 +35,12 @@ import com.android.settings.SettingsPreferenceFragment;
 public class PerformanceSettings extends SettingsPreferenceFragment {
     private static final String TAG = "PerformanceSettings";
 
+    private static final String PERF_PROFILE_PREF = "performance_profile";
     private static final String USE_16BPP_ALPHA_PREF = "pref_use_16bpp_alpha";
 
     private static final String USE_16BPP_ALPHA_PROP = "persist.sys.use_16bpp_alpha";
 
+    private Preference mPerfProfilePref;
     private CheckBoxPreference mUse16bppAlphaPref;
 
     private AlertDialog alertDialog;
@@ -45,34 +49,38 @@ public class PerformanceSettings extends SettingsPreferenceFragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        if (getPreferenceManager() != null) {
+        addPreferencesFromResource(R.xml.performance_settings);
 
-            addPreferencesFromResource(R.xml.performance_settings);
+        final Resources res = getResources();
+        PreferenceScreen prefSet = getPreferenceScreen();
 
-            PreferenceScreen prefSet = getPreferenceScreen();
-
-            mUse16bppAlphaPref = (CheckBoxPreference) prefSet.findPreference(USE_16BPP_ALPHA_PREF);
-            String use16bppAlpha = SystemProperties.get(USE_16BPP_ALPHA_PROP, "0");
-            mUse16bppAlphaPref.setChecked("1".equals(use16bppAlpha));
-
-            /* Display the warning dialog */
-            alertDialog = new AlertDialog.Builder(getActivity()).create();
-            alertDialog.setTitle(R.string.performance_settings_warning_title);
-            alertDialog.setMessage(getResources().getString(R.string.performance_settings_warning));
-            alertDialog.setButton(DialogInterface.BUTTON_POSITIVE,
-                    getResources().getString(com.android.internal.R.string.ok),
-                    new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog, int which) {
-                            return;
-                        }
-                    });
-            alertDialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
-                public void onCancel(DialogInterface dialog) {
-                    PerformanceSettings.this.finish();
-                }
-            });
-            alertDialog.show();
+        mPerfProfilePref = prefSet.findPreference(PERF_PROFILE_PREF);
+        String perfProfileProp = getString(R.string.config_perf_profile_prop);
+        if (mPerfProfilePref != null && TextUtils.isEmpty(perfProfileProp)) {
+            prefSet.removePreference(mPerfProfilePref);
         }
+
+        mUse16bppAlphaPref = (CheckBoxPreference) prefSet.findPreference(USE_16BPP_ALPHA_PREF);
+        String use16bppAlpha = SystemProperties.get(USE_16BPP_ALPHA_PROP, "0");
+        mUse16bppAlphaPref.setChecked("1".equals(use16bppAlpha));
+
+        /* Display the warning dialog */
+        alertDialog = new AlertDialog.Builder(getActivity()).create();
+        alertDialog.setTitle(R.string.performance_settings_warning_title);
+        alertDialog.setMessage(getResources().getString(R.string.performance_settings_warning));
+        alertDialog.setButton(DialogInterface.BUTTON_POSITIVE,
+                getResources().getString(com.android.internal.R.string.ok),
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        return;
+                    }
+                });
+        alertDialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
+            public void onCancel(DialogInterface dialog) {
+                PerformanceSettings.this.finish();
+            }
+        });
+        alertDialog.show();
     }
 
     @Override
